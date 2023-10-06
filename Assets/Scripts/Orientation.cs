f4ec320f7e196d62a025d461fff4c80e1e7b7b43
Fionn Edward Erickson <ericksoncooperation@gmail.com>
2023-10-06 13:57:01 +0200

    Some stuff

---
+++ Assets/Scripts/Orientation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientation : MonoBehaviour
+{   
+    private void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/highlight.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    private SpriteRenderer m_Sprite;
+    public SpriteRenderer m_ParentSprite;
+    public bool m_isActive = false;
+    // Start is called before the first frame update
+    
+    void Start()
+        m_Sprite.enable = false
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_isActive){
+            
+        }
+    }
+}
